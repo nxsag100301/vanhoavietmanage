88e3f384de9402186757d3f6adcd8d6ce7c40a9c
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-09 13:11:50 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <Button className='text-primary-600'>Nguyen Xuan Sang</Button>
+    </div>
+  )
+}
+
+export default App
